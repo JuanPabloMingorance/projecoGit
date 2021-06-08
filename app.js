9b5763727e5dccf45df6e66ff2655bd58f3907ce
JuanPabloMingorance <mingorancejuan5@gmail.com>
2021-06-08 18:40:59 -0300

    refactorizando el codigo

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
-const articulos = articulos.processrgv[2];
+const process = require('./process');
+const articulos = process.argv[2];
 
 const products = fs.readFileSync('./data/data.json','utf-8');
+
 
 console.log(products);
